45dfdc9b5252a471ae65ac36591636266389590d
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-05-17 18:31:16 +0700

    Update ketnoi.js

--- PhieuGiaoViec/ketnoi.js
+++ PhieuGiaoViec/ketnoi.js
@@ -11,7 +11,7 @@ var PGV_TEN_TRUONG_UP = "TRƯỜNG TH&THCS HỢP THÀNH";
 var PGV_TEN_PM = "Quản lý Nhiệm vụ";
 var PGV_TEN_PM_UP = "QUẢN LÝ CÔNG VIỆC";
 var PGV_PHIEN_BAN = "V2.3.2026";
-var PGV_TAC_GIA = "Thiết kế và phát triển\nHoàng Ngọc Lâm"; // Cập nhật đúng định dạng 2 dòng
+var PGV_TAC_GIA = "TPhát triển bởi\nHoàng Ngọc Lâm"; // Cập nhật đúng định dạng 2 dòng
 var PGV_GOOGLE_CLIENT_ID = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com";
 var PGV_MO_TA = "Hệ thống quản lý hiệu quả công việc nội bộ - " + PGV_TEN_TRUONG;
 
